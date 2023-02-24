438d343c9383e96ae60746cc9ac8737fe06b93d0
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-23 21:44:30 -0300

    Update AFAZENDA.py

--- AFAZENDA.py
+++ AFAZENDA.py
@@ -1,53 +1,77 @@
-import streamlink
-import subprocess
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
+import secrets 
 
-# Configuring Chrome options
-chrome_options = Options()
-chrome_options.add_argument("--window-size=1920,1080") # set screen size to 1920x1080
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
+# URL da página de login
+url_playplus_login = "https://www.playplus.com/Account/Login"
 
-# Instanciando o driver do Chrome
-driver = webdriver.Chrome(options=chrome_options)
+# Criando as opções para o chrome
+options = Options()
+options.add_argument("--window-size=1920,1080") # set screen size to 1920x1080
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
 
-# URL da página desejada
-url_site = "https://www.globo.com/"
+# Instanciando o driver do chrome
+driver = webdriver.Chrome(options=options)
 
-# Abrir a página desejada
-driver.get(url_site)
-
+# Abrir a página de login do PlayPlus
+driver.get(url_playplus_login)
 
 # Take 5 screenshots every 5 seconds
 for i in range(5):
     driver.save_screenshot(f"screenshot{i+1}.png")
     time.sleep(9)
-    
-    # Aguardar alguns segundos para carregar todo o conteúdo da página
+
+# Aguardar alguns segundos para carregar todo o conteúdo da página
 time.sleep(15)
 
-try:
-    # Obter o arquivo .m3u8
-    # Pegando o log de requisições da aba Rede
-    log_entries = driver.execute_script("return window.performance.getEntries();")
+# Esperar até que os campos de e-mail e senha estejam presentes na página
+email_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "UserName")))
 
-    # Buscando a primeira requisição que tem um arquivo .m3u8
-    for entry in log_entries:
-        if ".m3u8" in entry['name']:
-            print(entry['name'])
-            link = entry['name']
-            break
-    else:
-        print("No .m3u8 files found in network log entries")
+# Preencher o campo de e-mail com a informação desejada
+email_field.send_keys("oquefoiagora@hotmail.com")
 
-except Exception as e:
-    print("Error occurred while trying to obtain .m3u8 file:")
-    print(e)
+# Preencher o campo de senha com a informação desejada
+password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Password")))
+password_field.send_keys("ratosdeporao062")
 
-# Fechando o driver
+# Localizar e clicar no botão "Avançar"
+login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@class='redirect btn btn-primary btn-block btn-login btn-next']")))
+login_button.click()
+
+# Localizar e clicar no botão do perfil do usuário
+profile_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='profile redirect']")))
+profile_button.click()
+
+# Esperando a página ser carregada após clicar no perfil do usuário
+time.sleep(10)
+
+# URL da página desejada para extração da .m3u8
+url_playplus = "https://www.playplus.com/live/liveEvent/249"
+
+# Abrir a página desejada após o login
+driver.get(url_playplus)
+
+# Esperando o vídeo ser carregado e reproduzido
+time.sleep(5)
+
+# Obter o conteúdo da página
+html_content = driver.page_source
+
+# Encontrar o link .m3u8 na página
+start_index = html_content.find("var urlLive = '") + 15
+end_index = html_content.find("';", start_index)
+link = html_content[start_index:end_index]
+print(link)
+
+# Fechar o driver
 driver.quit()
+
+import os
+os.environ["EMAILPLAYPLUS"] = "seu-email-aqui"
+print(os.getenv("EMAILPLAYPLUS"))
